c0bde385ae1582e11bd5f530a41d7ec6de998dc4
Matias Viaggio <matiasviaggio59@gmail.com>
2022-04-29 00:42:54 -0300

    version BETA 1.1

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appBlog',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/matia/Documents/GitHub/ProyectoFinalViaggio/appBlog/templates/appBlog'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
