35c056fbe9e7f58529fc93a9aaeb90055627e04b
Matias Viaggio <matiasviaggio59@gmail.com>
2022-05-02 20:29:36 -0300

    version BETA 1.2

--- appBlog/models.py
+++ appBlog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Reservas(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    correo = models.EmailField()
+    evento = models.CharField(max_length=100)
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    correo = models.EmailField()
+    edad = models.IntegerField(default=0)
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    correo = models.EmailField()
+    edad = models.IntegerField(default=0)
+    duracion = models.IntegerField(default=3)
